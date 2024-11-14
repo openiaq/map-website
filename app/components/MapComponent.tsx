eb7313425a8edd25ed7059bba0920cfe2596669c
Pierre Wellner <pierre@pierrewellner.com>
2024-11-14 17:58:27 -0500

    Tested with different styles

--- app/components/MapComponent.tsx
+++ app/components/MapComponent.tsx
@@ -1,10 +1,21 @@
 
-"use client" 
+"use client"
 import * as React from 'react';
 import Map, { NavigationControl, ViewState } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 
-const handleMove = (evt: { viewState: ViewState }) => {
+const MAPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+const mapStyles = {
+  DEFAULT: "mapbox://styles/mapbox/streets-v9",
+  CARTO_LIGHT: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json",
+  CARTO_DARK: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+  AIRCODA: "mapbox://styles/pwellner/cld2519en001f01s497tr94se",
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function handleMove(evt: { viewState: ViewState }) {
   console.log(`>>>>> viewState=${JSON.stringify(evt.viewState)}`);
 };
 
@@ -16,11 +27,10 @@ export default function MapComponent() {
         latitude: 40,
         zoom: 3.5
       }}
-      onMove={handleMove}
+      //onMove={handleMove}
       style={{ width: '100%', height: '100vh' }}
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-
+      mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+      mapStyle={mapStyles.CARTO_LIGHT}
     >
       <NavigationControl position="top-left" />
     </Map>
--- app/config.ts
+++ app/config.ts
@@ -1,11 +1,12 @@
 // This approach to loading environment variables provides better error message handling when missing
 // https://www.austinshelby.com/blog/the-correct-way-to-load-environment-variables-in-nextjs
-//
-// But this Only works for build-time and server environment variables.
-// This does not work for browser run-time NEXT_PUBLIC... variables
-// which must instead be inlined as process.env.NEXT_PUBLIC...
 
-const getEnvironmentVariable = (environmentVariableName: string): string => {
+
+
+// Function only works for build-time and server environment variables.
+// This does not work for browser run-time NEXT_PUBLIC... variables,
+// which must instead be inlined as process.env.NEXT_PUBLIC...
+function getEnvironmentVariable(environmentVariableName: string): string {
   // Comes from process or file .env.local
   const environmentValue = process.env[environmentVariableName];
   if (!environmentValue) {
@@ -18,5 +19,5 @@ const getEnvironmentVariable = (environmentVariableName: string): string => {
 };
 
 export const config = {
-  DB_TOKEN: getEnvironmentVariable('DB_TOKEN')
+  DB_TOKEN: getEnvironmentVariable('DB_TOKEN'),
 };

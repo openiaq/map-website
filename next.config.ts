2f6442820e3e94f72e638edf88b41777f177b483
Pierre Wellner <pierre@pierrewellner.com>
2024-11-15 16:55:18 -0500

    Remove unused leftover NextJS files

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
-
-export default nextConfig;

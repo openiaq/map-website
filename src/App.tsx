1cc927a6d3ce95e7e5c9fdfa248588d9eb9e8012
Pierre Wellner <pierre@pierrewellner.com>
2024-11-26 18:06:46 -0500

    WIP ColorLegend component and function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import MapComponent, { LAYER_NAMES, mapStyles } from "./components/MapComponent";
+import ColorLegend, { ColorFromValueFunction, defaultColorFromValueFunction, colorBlindColorFromValueFunction } from './components/ColorLegend';
 //import { DarkModeSwitch } from 'react-toggle-dark-mode';
 import ToggleSwitch from './components/ToggleSwitch';
 import SlideInMenu from './components/SlideInMenu';
@@ -12,6 +13,7 @@ import { IoCart, IoCartOutline } from "react-icons/io5";
 export default function App() {
   const [mapStyle, setMapStyle] = useState("");
   const [layerNames, setLayerNames] = useState<string[]>(LAYER_NAMES);
+  const [colorFromValueFunction, setColorFromValueFunction] = useState<ColorFromValueFunction>(() => defaultColorFromValueFunction)
 
   const handleToggle = (newState: boolean, layerName: string) => {
     // setLayerNames must return new Array, or state change will be ignored
@@ -37,6 +39,7 @@ export default function App() {
           <MapComponent
             mapStyle={mapStyle}
             selectedLayerNames={layerNames}
+            colorFromValueFunction={colorFromValueFunction}
           />
           <SlideInMenu
             menuItems={menuItems}
@@ -80,8 +83,12 @@ export default function App() {
               topOffset='10rem'
             />
 
+            <div className="text-white bg-gray-800 bg-opacity-60 absolute bottom-5 right-1 p-1 rounded-md">
+              <ColorLegend
+                onSchemeSelection={colorFromValueFunction => setColorFromValueFunction(() => colorFromValueFunction)}
+              />
+            </div>
           </div>
-
         </div>
       </main>
       <footer className="">
---
+++ src/components/ColorLegend.tsx
@@ -0,0 +1,91 @@
+/*
+ * Component to display color/ppm legend that allows user to
+ * choose between conventional and color-blind schemes.
+ * 
+ * Callback provides selected function to parent component.
+ */
+
+import { useEffect, useState } from "react";
+import ToggleSwitch from "./ToggleSwitch";
+import { IoEye, IoEyeOutline } from "react-icons/io5";
+
+
+// Color schems from https://colorbrewer2.org/#type=diverging&scheme=BrBG&n=5
+type ColorScheme = [number, number, number][];
+const conventionalScheme: ColorScheme = [[26, 150, 65], [166, 217, 106], [255, 255, 191], [253, 174, 97], [215, 25, 28]];
+const colorBlindScheme: ColorScheme = [[44, 123, 182], [171, 217, 233], [255, 255, 191], [253, 174, 97], [215, 25, 28]];
+
+const thresholds = [600, 800, 1000, 1200]; // Must be sorted. TODO: Pass in as a prop?
+
+
+function colorFromValue(value: number, scheme: [number, number, number][], thresholds: number[]) {
+  if (scheme.length !== thresholds.length + 1) {
+    throw new Error("colorFromValue(): length of thresholds must be one less than length of scheme");
+  }
+
+  for (let i = 0; i < thresholds.length; i++) {
+    if (value < thresholds[i]) {
+      return scheme[i];
+    }
+  }
+  return scheme[thresholds.length];
+}
+
+
+export type ColorFromValueFunction = (value: number) => [number, number, number];
+export function defaultColorFromValueFunction(v: number) {
+  return colorFromValue(v, conventionalScheme, thresholds);
+}
+export function colorBlindColorFromValueFunction(v: number) {
+  return colorFromValue(v, colorBlindScheme, thresholds);
+}
+type ColorLegendProps = {
+  onSchemeSelection: (selectedFunction: ColorFromValueFunction) => void;
+};
+const ColorLegend: React.FC<ColorLegendProps> = ({ onSchemeSelection }) => {
+  const [isDefault, setIsDefault] = useState(true);
+
+  return (
+    <>
+      <ToggleSwitch
+        initialState={true}
+        hoverText="Colorblind"
+        onToggle={newState => {
+          setIsDefault(newState);
+          onSchemeSelection(newState ? defaultColorFromValueFunction : colorBlindColorFromValueFunction);
+        }}
+        offIcon={<IoEyeOutline className="w-6 h-8 text-gray-700" />}
+        onIcon={<IoEye className="w-6 h-8 text-gray-700" />}
+        topOffset="16rem"
+      />
+      <div className="absolute bottom-4 right-4 bg-gray-800 bg-opacity-60 p-2 rounded shadow-md">
+        {/*<h3 className="text-sm font-bold mb-1">CO2 PPM</h3>*/}
+        <div className="grid gap-1 items-center">
+          {[0, ...thresholds].map((threshold, index) => {
+            let label = "";
+            if (index === 0) {
+              label = "< " + thresholds[0];
+            }
+            else if (index === thresholds.length) {
+              label = "> " + thresholds[thresholds.length - 1]
+            }
+            else {
+              label = `${thresholds[index - 1]} - ${thresholds[index]}`
+            }
+            return (
+              <div key={threshold} className="flex">
+                <span
+                  className="w-4 h-4 rounded-full border border-black border-solid border-1"
+                  style={{ backgroundColor: `rgba(${(isDefault ? defaultColorFromValueFunction : colorBlindColorFromValueFunction)(threshold).join(',')})` }}
+                >
+                </span>
+                <span className="text-xs pl-2 whitespace-nowrap">{label}</span>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+export default ColorLegend;

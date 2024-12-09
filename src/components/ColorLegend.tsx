122a5614877b525f1945a23de5cf0b8610353eb8
Pierre Wellner <pierre@pierrewellner.com>
2024-12-09 09:43:41 -0500

    Tweak to colorblind toggle

--- src/components/ColorLegend.tsx
+++ src/components/ColorLegend.tsx
@@ -43,18 +43,6 @@ const ColorLegend: React.FC<ColorLegendProps> = ({ onSchemeSelection }) => {
 
   return (
     <div className="absolute bottom-4 right-2">
-      <ToggleSwitch
-        initialState={false}
-        hoverText="Colorblind"
-        onToggle={newState => {
-          setIsColorBlind(newState);
-          onSchemeSelection(newState);
-        }}
-        offIcon={<IoEyeOutline className="w-4 h-4 text-black" />}
-        onIcon={<IoEye className="w-4 h-4 text-black" />}
-        size={4}
-        className="absolute -top-0 -right-0"
-      />
       <div className="bg-gray-900 bg-opacity-70 p-2 rounded-md shadow-md">
         {/*<h3 className="text-sm font-bold mb-1">CO2 PPM</h3>*/}
         <div className="grid gap-1 items-center">
@@ -82,6 +70,19 @@ const ColorLegend: React.FC<ColorLegendProps> = ({ onSchemeSelection }) => {
           })}
         </div>
       </div>
+      <ToggleSwitch
+        initialState={false}
+        hoverText="Colorblind"
+        onToggle={newState => {
+          setIsColorBlind(newState);
+          onSchemeSelection(newState);
+        }}
+        offIcon={<IoEyeOutline className="w-4 h-4 text-black" />}
+        onIcon={<IoEye className="w-4 h-4 text-black" />}
+        size={4}
+        className="absolute -top-0 -right-1"
+      />
+
     </div>
   );
 }
